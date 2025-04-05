ad870916cb428e36c8165e155042bd3f914cb256
Станислав Маяцкий <stasm226@gmail.com>
2025-04-05 18:40:39 +0500

    Add periodic task for block inactive users

--- materials/tasks.py
+++ materials/tasks.py
@@ -8,13 +8,14 @@ from django.core.mail import send_mail
 from config.settings import EMAIL_HOST_USER
 from materials.models import Course
 
+
 logger = logging.getLogger(__name__)
 
 
 @shared_task
 def send_course_update_email(course_id):
     """
-    Отправляет уведомление об обновлении курса
+    Отправляет уведомление об обновлении курса.
     :param course_id: id курса
     :return: None
     """
@@ -37,10 +38,10 @@ def send_course_update_email(course_id):
     for sub in subscribers:
         if sub.user.email:
             send_mail(
-                subject,
-                message,
-                EMAIL_HOST_USER,
-                [sub.user.email],
+                subject=subject,
+                message=message,
+                from_email=EMAIL_HOST_USER,
+                recipient_list=[sub.user.email],
                 fail_silently=True
             )
             logger.info("Письмо отправлено пользователю %s" % sub.user)
